154221357410babbcb92bf85998d3e562faa6f9c
Alexander Ciesielski <alexander@ciesielski.co>
2020-08-15 09:24:30 +0200

    Initial commit

---
+++ config/database.js
@@ -0,0 +1,26 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+        ssl: { rejectUnauthorized: false },
+      },
+      options: {
+        pool: {
+          min: 0,
+          max: 10,
+          idleTimeoutMillis: 30000,
+          createTimeoutMillis: 30000,
+          acquireTimeoutMillis: 30000,
+        },
+      },
+    },
+  },
+});
